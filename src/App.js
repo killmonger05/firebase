e4c6738d3024cbaf9788eeffdc374cae0d7f40d4
Om Satam <omsatam2268@gmail.com>
2024-11-04 18:29:39 -0500

    App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useEffect, useState } from "react";
+import Auth from "../components/Auth";
+import TodoForm from "../components/TodoForm";
+import TodoList from "../components/TodoList";
+import { auth } from "../firebase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => setUser(currentUser));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? (
+        <>
+          <h1>Welcome, {user.email}</h1>
+          <button onClick={() => signOut(auth)}>Logout</button>
+          <TodoForm />
+          <TodoList />
+        </>
+      ) : (
+        <Auth />
+      )}
     </div>
   );
 }
